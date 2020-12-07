d541a99cfdd781b3777e9fefb0f912a43f9011bb
Muhd Rahiman <50654608+mdrhmn@users.noreply.github.com>
2020-12-07 15:06:32 +0800

    Updated settings.py (added template directory)

--- fcsit_survey_app/settings.py
+++ fcsit_survey_app/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'fcsit_survey_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
