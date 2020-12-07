51ace5c98f21251cec660840008871bb5afc9d5f
Muhd Rahiman <50654608+mdrhmn@users.noreply.github.com>
2020-12-07 19:20:20 +0800

    Created new Survey and Course models
    - Created multi-level Bootstrap pill-tabs at index.html
    - Created new Survey and Course models
    - Added new CSS file for index.html (index.css)
    - Excluded main/migrations folder in .gitignore

--- main/models.py
+++ main/models.py
@@ -1,3 +1,24 @@
 from django.db import models
-
+from django.core.validators import MaxValueValidator
 # Create your models here.
+
+class Survey(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
+    year = models.PositiveIntegerField(default=0, validators=[MaxValueValidator(3)])
+    course_code = models.ForeignKey("Course", on_delete=models.CASCADE) 
+    link = models.CharField(max_length=200)
+    PIC_name = models.CharField(max_length=100)
+    PIC_email = models.CharField(max_length=100)
+    approval = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+class Course(models.Model):
+    code = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+    year = models.PositiveIntegerField(default=0, validators=[MaxValueValidator(3)])
+
+    def __str__(self):
+            return self.code
--- main/views.py
+++ main/views.py
@@ -3,7 +3,14 @@ from .models import *
 # Create your views here.
 
 def home(request):
-    return render(request, 'index.html')
+
+    context = {
+        'survey_list': Survey.objects.all(),
+    }
+
+    # print(Survey.objects.all().name)
+
+    return render(request, 'index.html', context)
 
 def about_us(request):
     return render(request, 'about_us.html')
