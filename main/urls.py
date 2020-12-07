c6782618690a387b7e1d8243eefa611e0fa7272e
Muhd Rahiman <50654608+mdrhmn@users.noreply.github.com>
2020-12-07 23:48:23 +0800

    Added extra empty URL to redirect to home

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('home/', views.home, name='home'),
     path('about_us/', views.about_us, name='about_us'),
     path('whatsapp_api/', views.whatsapp_api, name='whatsapp_api'),
