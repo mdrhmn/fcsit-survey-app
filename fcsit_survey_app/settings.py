a42ba6c443fb81b54eb515e4be2322a520996b11
Muhd Rahiman <50654608+mdrhmn@users.noreply.github.com>
2020-12-07 23:36:54 +0800

    Changed ALLOWED_HOSTS from * (allow all) to fcsit-survey-app.herokuapp.com

--- fcsit_survey_app/settings.py
+++ fcsit_survey_app/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = '1n#w!0nafu6i(!b7(19+v$5w5(ro7633=fxltzap!s)19hw$c%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://fcsit-survey-app.herokuapp.com/']
 
 
 # Application definition
